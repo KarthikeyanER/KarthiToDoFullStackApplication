ad4f92194d839aa88ac7e5bb28be1268d3cac0a4
KarthikeyanER <130287148+KarthikeyanER@users.noreply.github.com>
2024-02-14 13:36:15 +0530

    Delete BackEnd/ToDo directory

--- BackEnd/ToDo/src/main/java/com/gl/todo/repository/ToDoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gl.todo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gl.todo.model.ToDo;
-
-public interface ToDoRepository extends JpaRepository<ToDo, Integer>{
-
-}
